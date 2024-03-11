310c0b0855929bba84ebd0f305a2d1cc844d8d0b
Shabbir Saiyad <shabbirsaiyad83@gmail.com>
2024-03-11 17:14:02 +0530

    find shop by city done

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -1,8 +1,13 @@
 import express from "express"
 import { signup } from "../controllers/user.js";
+import { getShopByCity } from "../controllers/Vendor.js";
 
 const router = express.Router();
 
 router.post("/new", signup)
+
+
+
+router.post("/getShopByCity", getShopByCity);
 
 export default router;
